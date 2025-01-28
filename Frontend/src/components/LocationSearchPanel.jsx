5f462f1bac135af5dc9f3a9bed94db3d5fcd6239
ChetanSrikant <chetansmandiga@gmail.com>
2025-01-28 17:37:57 +0530

    vehicle selection phase-1

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from 'react';
 
 const LocationSearchPanel = () => {
   return (
     <div>
-      sample ra munda
-    </div>
-  )
-}
+      {/* sample ra munda */}
+      <div className='border-2 p-3 rounded-xl flex items-center justify-start gap-2 mt-2 border-grey-50 active:border-black'>
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21c4.97-4.97 8-9 8-12a8 8 0 10-16 0c0 3 3.03 7.03 8 12z" />
+            <circle cx="12" cy="9" r="2.5" fill="currentColor" />
+          </svg>
+        </h2>
+        <h4>14-42 p ant colony dilshukhnagar hyderabad</h4>
+      </div>
 
-export default LocationSearchPanel
+      <div className='border-2 p-3 rounded-xl flex items-center justify-start gap-2 mt-2 border-grey-50 active:border-black'>
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21c4.97-4.97 8-9 8-12a8 8 0 10-16 0c0 3 3.03 7.03 8 12z" />
+            <circle cx="12" cy="9" r="2.5" fill="currentColor" />
+          </svg>
+        </h2>
+        <h4>14-42 p ant colony dilshukhnagar hyderabad</h4>
+      </div>
+
+      <div className='border-2 p-3 rounded-xl flex items-center justify-start gap-2 mt-2 border-grey-50 active:border-black'>
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21c4.97-4.97 8-9 8-12a8 8 0 10-16 0c0 3 3.03 7.03 8 12z" />
+            <circle cx="12" cy="9" r="2.5" fill="currentColor" />
+          </svg>
+        </h2>
+        <h4>14-42 p ant colony dilshukhnagar hyderabad</h4>
+      </div>
+
+      <div className='border-2 p-3 rounded-xl flex items-center justify-start gap-2 mt-2 border-grey-50 active:border-black'>
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21c4.97-4.97 8-9 8-12a8 8 0 10-16 0c0 3 3.03 7.03 8 12z" />
+            <circle cx="12" cy="9" r="2.5" fill="currentColor" />
+          </svg>
+        </h2>
+        <h4>14-42 p ant colony dilshukhnagar hyderabad</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
   const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
   const panelRef = useRef(null);
-  const panelCloseRef = useRef(null)
+  const panelCloseRef = useRef(null);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -19,28 +19,28 @@ const Home = () => {
       if (panelOpen) {
         gsap.to(panelRef.current, {
           height: "100%",
-          opacity:1,
-          padding:20
+          opacity: 1,
+          padding: 20,
         });
-        gsap.to(panelCloseRef.current,{
-          opacity:1
-        })
+        gsap.to(panelCloseRef.current, {
+          opacity: 1,
+        });
       } else {
         gsap.to(panelRef.current, {
           height: "0%",
-          opacity:0,
-          padding:0
+          opacity: 0,
+          padding: 0,
         });
-        gsap.to(panelCloseRef.current,{
-          opacity:0
-        })
+        gsap.to(panelCloseRef.current, {
+          opacity: 0,
+        });
       }
     },
     [panelOpen]
   );
 
   return (
-    <div className="h-screen relative">
+    <div className="h-screen relative overflow-hidden">
       <img className="w-20 absolute left-5 top-5" src="Zap.png" alt="" />
       <div className="h-screen w-screen">
         <img
@@ -51,11 +51,27 @@ const Home = () => {
       </div>
       <div className="flex flex-col justify-end h-screen absolute w-full top-0">
         <div className="h-[30%] p-5 bg-white relative">
-          <h5 ref={panelCloseRef} onClick={()=>{
-            setPanelOpen(false)
-          }} className="absolute opacity-0 top-6 right-6">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
-              <path d="M6 9l6 6 6-6" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          <h5
+            ref={panelCloseRef}
+            onClick={() => {
+              setPanelOpen(false);
+            }}
+            className="absolute opacity-0 top-6 right-6"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 9l6 6 6-6"
+                stroke="black"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
             </svg>
           </h5>
           <h4 className="text-2xl font-semibold">Find a trip</h4>
@@ -87,6 +103,57 @@ const Home = () => {
         <div ref={panelRef} className="h-[70%] bg-white h-0">
           <LocationSearchPanel></LocationSearchPanel>
         </div>
+      </div>
+      <div className="fixed w-full bg-white z-10 bottom-0 translate-y-full px-3 py-8">
+        <h3 className="text-2xl font-semibold mb-5">Choose a Vehicle</h3>
+        <div className="flex w-full border-2 mb-2 active:border-black rounded-xl items-center justify-between p-3">
+          <img
+            className="h-16"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_956,h_638/v1682350114/assets/c2/296eac-574a-4a81-a787-8a0387970755/original/UberBlackXL.png"
+            alt=""
+          />
+          <div className="ml-2 w-1/2">
+           <h4 className="font-medium text-base flex items-center gap-1">Cab 
+            <span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4" > <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.33 0-8 1.67-8 5v2h16v-2c0-3.33-4.67-5-8-5z" /> </svg> </span> 4 </h4>
+
+           <h5  className="font-medium text-sm">2 mins away</h5>
+            <p  className="font-normal text-xs text-gray-600">Affordable Compact rides</p>
+          </div>
+          <h2 className="text-lg font-semibold">₹193.20</h2>
+        </div>
+
+        <div className="flex w-full border-2 mb-2 active:border-black rounded-xl items-center justify-between p-3">
+          <img
+            className="h-16"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+            alt=""
+          />
+          <div className="-ml-4 w-1/2">
+           <h4 className="font-medium text-base flex items-center gap-1">Bike 
+            <span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4" > <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.33 0-8 1.67-8 5v2h16v-2c0-3.33-4.67-5-8-5z" /> </svg> </span> 1 </h4>
+
+           <h5  className="font-medium text-sm">2 mins away</h5>
+            <p  className="font-normal text-xs text-gray-600">Affordable Motercycle rides</p>
+          </div>
+          <h2 className="text-xtext-lg font-semibold">₹65</h2>
+        </div>
+
+        <div className="flex w-full border-2 mb-2 active:border-black rounded-xl items-center justify-between p-3">
+          <img
+            className="h-16"
+            src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+            alt=""
+          />
+          <div className="ml-2 w-1/2">
+           <h4 className="font-medium text-base flex items-center gap-1">Auto 
+            <span> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4" > <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-3.33 0-8 1.67-8 5v2h16v-2c0-3.33-4.67-5-8-5z" /> </svg> </span> 3 </h4>
+
+           <h5  className="font-medium text-sm">2 mins away</h5>
+            <p  className="font-normal text-xs text-gray-600">Affordable Auto rides</p>
+          </div>
+          <h2 className="text-xtext-lg font-semibold">₹118.60</h2>
+        </div>
+
       </div>
     </div>
   );
